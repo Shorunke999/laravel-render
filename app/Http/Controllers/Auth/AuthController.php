d4c492b9e80b911f2d820ee23a0095535cf01e2a
Shorunke999 <134306919+Shorunke999@users.noreply.github.com>
2025-04-05 04:56:29 -0700

    correcting anding log

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -18,45 +18,52 @@ class AuthController extends Controller
      */
     public function register(Request $request)
     {
-        $validator = Validator::make($request->all(), [
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:8|confirmed',
-            'type' => 'required|in:customer,admin'
-        ]);
+        try {
+            $validator = Validator::make($request->all(), [
+                'name' => 'required|string|max:255',
+                'email' => 'required|string|email|max:255|unique:users',
+                'password' => 'required|string|min:8|confirmed',
+                'type' => 'required|in:customer,admin'
+            ]);
 
-        if ($validator->fails()) {
+            if ($validator->fails()) {
+                return response()->json([
+                    'errors' => $validator->errors()
+                ], 422);
+            }
+
+            $validatedData = $validator->validated();
+
+            if(User::where('email',$validatedData['email'])->first())
+            {
+                return response()->json([
+                    'message' =>  'Email '.$validatedData['email'].' has already been taken',
+                ], 400);
+            }
+
+            $user = User::create([
+                'name' => $validatedData['name'],
+                'email' => $validatedData['email'],
+                'password' => Hash::make($validatedData['password']),
+                //'type' => $validatedData['type']
+                'type' => strtolower($validatedData['type'])
+            ]);
+
+            // Create token for the newly registered user
+            $token = $user->createToken('auth_token')->plainTextToken;
+
             return response()->json([
-                'errors' => $validator->errors()
-            ], 422);
-        }
-
-        $validatedData = $validator->validated();
-
-        if(User::where('email',$validatedData['email'])->first())
-        {
+                'message' => 'User registered successfully',
+                'user' => new UserResource($user),
+                'access_token' => $token,
+                'token_type' => 'Bearer'
+            ], 200);
+        } catch (\Exception $e) {
             return response()->json([
-                'message' =>  'Email '.$validatedData['email'].' has already been taken',
-            ], 400);
+                'message' => 'Error during Registation ',
+                'error' => $e->getMessage()
+            ], 500);
         }
-
-        $user = User::create([
-            'name' => $validatedData['name'],
-            'email' => $validatedData['email'],
-            'password' => Hash::make($validatedData['password']),
-            //'type' => $validatedData['type']
-            'type' => strtolower($validatedData['type'])
-        ]);
-
-        // Create token for the newly registered user
-        $token = $user->createToken('auth_token')->plainTextToken;
-
-        return response()->json([
-            'message' => 'User registered successfully',
-            'user' => new UserResource($user),
-            'access_token' => $token,
-            'token_type' => 'Bearer'
-        ], 201);
     }
 
     /**
@@ -64,40 +71,52 @@ class AuthController extends Controller
      */
     public function login(Request $request)
     {
-        $validator = Validator::make($request->all(), [
-            'email' => 'required|email',
-            'type' => 'required|in:customer,admin',
-            'password' => 'required',
-        ]);
-
-        if ($validator->fails()) {
-            return response()->json([
-                'errors' => $validator->errors()
-            ], 422);
-        }
-
-        $user = User::where('email', $request->email)
-                    ->where('type', $request->type)
-                    ->first();
-
-        if (!$user || !Hash::check($request->password, $user->password)) {
-            throw ValidationException::withMessages([
-                'email' => ['The provided credentials are incorrect.'],
+        try {
+            $validator = Validator::make($request->all(), [
+                'email' => 'required|email',
+                'type' => 'required|in:customer,admin',
+                'password' => 'required',
             ]);
+
+            if ($validator->fails()) {
+                return response()->json([
+                    'errors' => $validator->errors()
+                ], 422);
+            }
+
+            $user = User::where('email', $request->email)
+                        ->where('type', $request->type)
+                        ->first();
+
+            if (!$user || !Hash::check($request->password, $user->password)) {
+                throw ValidationException::withMessages([
+                    'email' => ['The provided credentials are incorrect.'],
+                ]);
+            }
+
+            // Revoke previous tokens (optional)
+            // $user->tokens()->delete();
+
+            // Create new token
+            $token = $user->createToken('auth_token')->plainTextToken;
+
+            return response()->json([
+                'message' => 'Login successful',
+                'user' => new UserResource($user),
+                'access_token' => $token,
+                'token_type' => 'Bearer'
+            ]);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'message' => 'Error Logging in',
+                'error' => $th->getMessage()
+            ], 422);
+        }catch (\Exception $e) {
+            return response()->json([
+                'message' => 'Error Logging in',
+                'error' => $e->getMessage()
+            ], 500);
         }
-
-        // Revoke previous tokens (optional)
-        // $user->tokens()->delete();
-
-        // Create new token
-        $token = $user->createToken('auth_token')->plainTextToken;
-
-        return response()->json([
-            'message' => 'Login successful',
-            'user' => new UserResource($user),
-            'access_token' => $token,
-            'token_type' => 'Bearer'
-        ]);
     }
 
     /**
