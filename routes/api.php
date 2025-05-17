7bf9e4664636a64daad098ed318ec254fdc91f03
Shorunke999 <134306919+Shorunke999@users.noreply.github.com>
2025-05-16 22:33:19 -0700

    payment middleware removed

--- routes/api.php
+++ routes/api.php
@@ -12,12 +12,10 @@ use App\Http\Controllers\ReviewController;
 use App\Http\Controllers\UtilityController;
 use App\Http\Controllers\WishlistController;
 use App\Http\Middleware\CheckAdmin;
-use App\Http\Middleware\CheckPayment;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware([CheckPayment::class])->group(function(){
-     /* Route::get('/testing_deployment',function()
+/* Route::get('/testing_deployment',function()
     {
         return 'deployment successfull';
     });
@@ -26,24 +24,25 @@ Route::middleware([CheckPayment::class])->group(function(){
     Artisan::call('migrate', ['--force' => true]);
     return 'Migrations ran successfully!';
 });*/
-    Route::get('/login', [AuthController::class, 'Unauthorized'])->name('login');
-    Route::post('register', [AuthController::class, 'register']);
-    Route::post('login', [AuthController::class, 'login']);
 
-    // Optional: Email Verification Routes
-    // Route::get('verify-email/{token}', [AuthController::class, 'verifyEmail']);
+Route::get('/login', [AuthController::class, 'Unauthorized'])->name('login');
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
 
-    Route::get('/categories', [CategoryController::class, 'index']);  // List all categories
-    Route::get('/categories/{category}', [CategoryController::class, 'show']); // Get a single category
+// Optional: Email Verification Routes
+// Route::get('verify-email/{token}', [AuthController::class, 'verifyEmail']);
+
+Route::get('/categories', [CategoryController::class, 'index']);  // List all categories
+Route::get('/categories/{category}', [CategoryController::class, 'show']); // Get a single category
 
 
-    // Artwork Routes
-    Route::get('/artworks', [ArtworkController::class, 'index']);
-    Route::get('/artworks/search', [ArtworkController::class, 'search']);
-    Route::get('/artworks/{artwork}', [ArtworkController::class, 'show']);
+// Artwork Routes
+Route::get('/artworks', [ArtworkController::class, 'index']);
+Route::get('/artworks/search', [ArtworkController::class, 'search']);
+Route::get('/artworks/{artwork}', [ArtworkController::class, 'show']);
 
 Route::middleware('auth:sanctum')->group(function () {
-     // Get authenticated user
+    // Get authenticated user
     Route::get('me', [AuthController::class, 'me']);
     Route::post('log-out', [AuthController::class, 'logout']);
     //Category Routes
@@ -60,7 +59,7 @@ Route::middleware('auth:sanctum')->group(function () {
         Route::put('/artworks/{artwork}', [ArtworkController::class, 'update']);
         Route::delete('/artworks/{artwork}', [ArtworkController::class, 'destroy']);
 
-         //Order Update
+        //Order Update
         Route::post('/orders/{order}/update', [OrderController::class, 'update']);
 
         // Admin Stats
@@ -68,10 +67,8 @@ Route::middleware('auth:sanctum')->group(function () {
 
         // Feedback listing
         Route::get('/feedbacks', [FeedbackController::class, 'index']);
-        Route::put('/feedbacks/{feedback}', [FeedbackController::class,'update']);
+        Route::put('/feedbacks/{feedback}', [FeedbackController::class, 'update']);
         Route::post('/feedback/mark-all-read', [FeedbackController::class, 'markAllAsRead']);
-
-
     });
 
 
@@ -102,25 +99,22 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/orders/{order}/cancel', [OrderController::class, 'cancel']);
 
 
-     // View wishlist
-     Route::get('/wishlist', [WishlistController::class, 'index']);
-     // Add artwork to wishlist
-     Route::post('/wishlist/{artworkId}', [WishlistController::class, 'store']);
-     // Remove artwork from wishlist
-     Route::delete('/wishlist/{artworkId}', [WishlistController::class, 'destroy']);
+    // View wishlist
+    Route::get('/wishlist', [WishlistController::class, 'index']);
+    // Add artwork to wishlist
+    Route::post('/wishlist/{artworkId}', [WishlistController::class, 'store']);
+    // Remove artwork from wishlist
+    Route::delete('/wishlist/{artworkId}', [WishlistController::class, 'destroy']);
 
     //payment
-    Route::post('/paystack/checkout',[PaystackController::class, 'initiateTransaction'])->name('payment.process');
-    Route::get('/paystack/disable/recurring/charge',[PaystackController::class, 'diableRecurringCharge'])->name('disable.recurring');
+    Route::post('/paystack/checkout', [PaystackController::class, 'initiateTransaction'])->name('payment.process');
+    Route::get('/paystack/disable/recurring/charge', [PaystackController::class, 'diableRecurringCharge'])->name('disable.recurring');
 
     //feedback
-    Route::post('/feedback',[FeedbackController::class, 'store'])->name('feedback');
-
+    Route::post('/feedback', [FeedbackController::class, 'store'])->name('feedback');
 });
 Route::post('/newsletter/subscribe', [UtilityController::class, 'subscribeNewsletter']);
-Route::post('/webhook/verify',[PaystackController::class, 'processWebhook']);
+Route::post('/webhook/verify', [PaystackController::class, 'processWebhook']);
 // Password Reset Routes
 Route::post('/forgot-password', [PasswordResetController::class, 'forgotPassword']);
 Route::post('/reset-password', [PasswordResetController::class, 'resetPassword']);
-
-});
